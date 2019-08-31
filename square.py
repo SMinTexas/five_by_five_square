d01d02fd5d52988c4592481d323b5c57798b701a
Steven Murray <tstevenm@gmail.com>
2019-08-30 19:35:22 -0500

    Initial Commit

---
+++ square.py
@@ -0,0 +1,11 @@
+# Print a 5x5 square of * characters.
+
+def print_square(size_of_square, to_print):
+    for number in range(0, size_of_square):
+        print(size_of_square * to_print)
+
+print_square(5,'*')
+
+
+
+
